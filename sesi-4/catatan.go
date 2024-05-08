a47d2bf96c33ccf4121f4db9f3f482343baca661
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2024-05-08 14:14:27 +0800

    do mini challenge 4.1: unordered

---
+++ sesi-4/catatan.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type hitung interface {
+	luas() float64
+	keliling() float64
+}
+
+type persegi struct {
+	sisi float64
+}
+
+type lingkaran struct {
+	diameter, jariJari float64
+}
+
+func (l lingkaran) luas() float64 {
+	return math.Pi * math.Pow(l.jariJari, 2)
+}
+
+func (p persegi) luas() float64 {
+	return math.Pow(p.sisi, 2)
+}
+
+func (l lingkaran) keliling() float64 {
+	return math.Pi * l.diameter
+}
+
+func (p persegi) keliling() float64 {
+	return p.sisi
+}
+
+func catatan() {
+
+	var lingkaran1 hitung = lingkaran{14, 7}
+	var persegi1 hitung = persegi{5}
+
+	fmt.Println("Luas lingkaran		: ", lingkaran1.luas())
+	fmt.Println("Keliling lingkaran	: ", lingkaran1.keliling())
+	fmt.Println("Luas persegi		: ", persegi1.luas())
+	fmt.Println("Keliling persegi	: ", persegi1.keliling())
+
+	// fmt.Printf("Type of lingkaran: %T \n", lingkaran1)
+	// fmt.Printf("Type of persegi: %T \n", persegi1)
+}
---
+++ sesi-4/challenge-unordered.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type aksi interface {
+	coba() int
+	bisa() int
+}
+
+type Angka struct {
+	angka int
+}
+
+func (j Angka) coba() int {
+	return j.angka
+}
+
+func (j Angka) bisa() int {
+	return j.angka
+}
+
+func challengeUnordered() {
+	fmt.Println("--- start ---")
+
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 4; i++ {
+		wg.Add(1)
+		go fungsiSatu(&wg, i)
+	}
+
+	for j := 1; j <= 4; j++ {
+		wg.Add(1)
+		go fungsiDua(&wg, j)
+	}
+
+	wg.Wait()
+}
+
+func fungsiSatu(wg *sync.WaitGroup, i int) {
+	defer wg.Done()
+
+	var myAction aksi = Angka{i}
+	fmt.Println("[coba]", myAction.coba())
+
+}
+
+func fungsiDua(wg *sync.WaitGroup, i int) {
+	defer wg.Done()
+
+	var myAction aksi = Angka{i}
+	fmt.Println("[bisa]", myAction.bisa())
+
+}
