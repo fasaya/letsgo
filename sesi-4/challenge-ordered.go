a61f651b5d34b7e298f97b8691b9aaaf6f65a947
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2024-05-14 15:17:27 +0800

    update mini challenge 4

--- sesi-4/challenge-ordered.go
+++ sesi-4/challenge-ordered.go
@@ -2,60 +2,42 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
 )
 
-type aksi2 interface {
-	coba() string
-	bisa() string
-}
-
-type Angka2 struct {
-	angka int
-}
-
-func (j Angka2) coba() string {
-	return "[coba] " + strconv.Itoa(j.angka)
-}
-
-func (j Angka2) bisa() string {
-	return "[bisa] " + strconv.Itoa(j.angka)
-}
-
 func challengeOrdered() {
-	fmt.Println("--- Ordered ---")
+	fmt.Println("--- Odered ---")
 
 	var wg sync.WaitGroup
 	var mu sync.Mutex
 
-	for i := 1; i <= 4; i++ {
-		wg.Add(2)
-		go fungsiSatuOrdered(&wg, i, &mu)
-		go fungsiDuaOrdered(&wg, i, &mu)
+	firstInterface := []interface{}{1, 2, 3, 4}
+	secondInterface := []interface{}{1, 2, 3, 4}
+
+	for i := 0; i <= 3; i++ {
+		wg.Add(1)
+		idx := i
+
+		go func() {
+			defer wg.Done()
+			mu.Lock()
+			fmt.Println("[coba]", firstInterface[idx])
+			mu.Unlock()
+		}()
+
+	}
+
+	for j := 0; j <= 3; j++ {
+		wg.Add(1)
+		jdx := j
+
+		go func() {
+			defer wg.Done()
+			mu.Lock()
+			fmt.Println("[bisa]", secondInterface[jdx])
+			mu.Unlock()
+		}()
 	}
 
 	wg.Wait()
-}
-
-func fungsiSatuOrdered(wg *sync.WaitGroup, i int, mu *sync.Mutex) {
-	defer wg.Done()
-
-	mu.Lock()
-
-	var myAction aksi2 = Angka2{i}
-	fmt.Println(myAction.coba())
-
-	mu.Unlock()
-}
-
-func fungsiDuaOrdered(wg *sync.WaitGroup, i int, mu *sync.Mutex) {
-	defer wg.Done()
-
-	mu.Lock()
-
-	var myAction aksi2 = Angka2{i}
-	fmt.Println(myAction.bisa())
-
-	mu.Unlock()
 }
--- sesi-4/challenge-unordered.go
+++ sesi-4/challenge-unordered.go
@@ -2,53 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
 )
-
-type aksi interface {
-	coba() string
-	bisa() string
-}
-
-type Angka struct {
-	angka int
-}
-
-func (j Angka) coba() string {
-	return "[coba] " + strconv.Itoa(j.angka)
-}
-
-func (j Angka) bisa() string {
-	return "[bisa] " + strconv.Itoa(j.angka)
-}
 
 func challengeUnordered() {
 	fmt.Println("--- Unordered ---")
 
 	var wg sync.WaitGroup
 
-	for i := 1; i <= 4; i++ {
+	firstInterface := []interface{}{1, 2, 3, 4}
+	secondInterface := []interface{}{1, 2, 3, 4}
+
+	for i := 0; i <= 3; i++ {
 		wg.Add(2)
-		go fungsiSatu(&wg, i)
-		go fungsiDua(&wg, i)
+
+		idx := i
+
+		go func() {
+			defer wg.Done()
+			fmt.Println("[coba]", firstInterface[idx])
+		}()
+
+		go func() {
+			defer wg.Done()
+			fmt.Println("[bisa]", secondInterface[idx])
+		}()
+
 	}
 
 	wg.Wait()
-}
-
-func fungsiSatu(wg *sync.WaitGroup, i int) {
-	defer wg.Done()
-
-	var myAction aksi = Angka{i}
-	fmt.Println(myAction.coba())
-
-}
-
-func fungsiDua(wg *sync.WaitGroup, i int) {
-	defer wg.Done()
-
-	var myAction aksi = Angka{i}
-	fmt.Println(myAction.bisa())
-
 }
